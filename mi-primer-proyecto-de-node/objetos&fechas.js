c9ec7721d7e697c25e0b288e045ab7f67820f885
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-03-11 10:27:59 -0300

    //ejercicio objetos y fechas

---
+++ mi-primer-proyecto-de-node/objetos&fechas.js
@@ -0,0 +1,22 @@
+
+const yoPersona = {
+  nombre: "Oriana",
+  nombre2: "Soledad",
+  apellido: "Galindez",
+  edad: 30,
+  desarrolladora: true,
+  libroFavorito: {
+    titulo: "mi planta naranja lima",
+    autor: "Jose Mauro de Vasconcelos",
+    editorial: "El Ateneo",
+    anioPublicacion: 1968,
+  },
+  nacimiento: {
+    fecha: new Date("january 20 1993"),
+    lugar: 'Argentina',
+    isCapitalFederal:true
+  },
+};
+
+
+
